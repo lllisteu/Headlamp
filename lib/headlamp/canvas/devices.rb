7e44e9c2ee9f9c469c159c4a7cbeb4d7b66a4743
lllisteu <lllisteu@users.noreply.github.com>
2019-07-04 10:03:33 +0200

    Add device handling to Canvas

--- lib/headlamp/canvas/devices.rb
+++ lib/headlamp/canvas/devices.rb
@@ -2,9 +2,35 @@ module Headlamp
 
   class Canvas
 
+    def automatic=(v)
+      @automatic = !!v
+    end
+
+    def automatic
+      @automatic = true unless (defined? @automatic)
+      @automatic
+    end
+
+    def devices
+      @dev ||= []
+    end
+
+    def add_device(d)
+      devices.push d
+    end
+
+    def render
+      devices.each do |d|
+        if d.respond_to? :render
+          d.render self, {}
+        end
+      end
+    end
+
     private
 
     def automatically_render
+      render if automatic
     end
 
   end
---
+++ test/tests/test_canvas_devices.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+
+require 'headlamp/canvas'
+
+class Test_canvas_devices < Test::Unit::TestCase
+
+  def test_auto
+    sheet = Headlamp::Canvas.new
+    assert_true sheet.automatic
+
+    sheet.automatic = nil
+    assert_false sheet.automatic
+
+    assert_equal [], sheet.devices
+    sheet.add_device Object.new
+    assert_equal 1, sheet.devices.count
+  end
+
+end
