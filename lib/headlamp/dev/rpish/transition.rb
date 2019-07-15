462d4157bd6deb6ae737c023b250f64dd2acca5a
lllisteu <lllisteu@users.noreply.github.com>
2019-07-15 14:31:48 +0200

    Add 'dissolve' transition

--- lib/headlamp/dev/rpish/transition.rb
+++ lib/headlamp/dev/rpish/transition.rb
@@ -7,6 +7,15 @@ module Headlamp
         poke get_canvas(canvas,params).flatten.map(&:rgb565).join
       end
 
+      def transition__dissolve(canvas,params)
+        old = peek.scan(/.{2}/)
+        new = get_canvas(canvas,params).flatten.map(&:rgb565)
+
+        changes = (0..63).reject { |i| new[i] == old[i] }
+
+        changes.shuffle.each { |i| nap 0.01; poke i, new[i] }
+      end
+
     end
 
   end
