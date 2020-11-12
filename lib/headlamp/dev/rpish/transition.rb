add9d3e3987eaaa2e8438cb37dedbd26e7c381e7
lllisteu <lllisteu@users.noreply.github.com>
2020-11-12 09:37:11 +0100

    Add 'swirl' transition to RPiSH.

--- lib/headlamp/dev/rpish/transition.rb
+++ lib/headlamp/dev/rpish/transition.rb
@@ -32,18 +32,57 @@ module Headlamp
         poke_blocks(index.transpose.reverse, canvas, params)
       end
 
+      def transition__swirl(canvas,params)
+        poke_blocks(
+      [
+        [ 0, 63],
+        [ 1, 62],
+        [ 2, 61],
+        [ 3, 60],
+        [ 4, 59],
+        [ 5, 58],
+        [ 6, 57],
+        [ 7, 56],
+        [15, 48],
+        [23, 40],
+        [31, 32],
+        [39, 24],
+        [47, 16],
+        [55,  8],
+        [54,  9],
+        [53, 10],
+        [52, 11],
+        [51, 12],
+        [50, 13],
+        [49, 14],
+        [41, 22],
+        [33, 30],
+        [25, 38],
+        [17, 46],
+        [18, 45],
+        [19, 44],
+        [20, 43],
+        [21, 42],
+        [29, 34],
+        [37, 26],
+        [36, 27],
+        [35, 28],
+      ].reverse,
+          canvas, params)
+      end
+
       private
 
       def poke_blocks(blocks,canvas,params)
-        new = raw_frame(canvas,params)
+        frame = raw_frame(canvas,params)
         blocks.each do |b|
-          b.each { |p| poke p, new[p] }
+          b.each { |p| poke p, frame[p] }
           nap 0.05
         end
       end
 
       def raw_frame(canvas,params)
-        new = get_canvas(canvas,params).flatten.map(&:rgb565)
+        get_canvas(canvas,params).flatten.map(&:rgb565)
       end
 
     end
